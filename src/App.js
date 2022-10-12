0589bcf65aaebc6f1b2cb5f99d23a77fa1ad4733
ayushakash <ayushakash9@gmai.com>
2022-10-13 00:22:18 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React, { useRef } from 'react';
+import { MapContainer, TileLayer,Marker,Popup } from 'react-leaflet';
 import './App.css';
+import 'leaflet/dist/leaflet.css';
+
+const defaultCenter = [12.910992237810687, 77.63791637508609];
+const defaultZoom = 18;
 
 function App() {
+  const mapRef = useRef();
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MapContainer ref={mapRef} center={defaultCenter} zoom={defaultZoom}>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="&copy; <a href=&quot;https://www.openstreetmap.org/copyright&quot;>OpenStreetMap</a> contributors" />
+        <Marker position={defaultCenter}>
+        <Popup>
+        Corvid Systems.
+      </Popup>
+        </Marker>
+      </MapContainer>
+      
     </div>
   );
 }
